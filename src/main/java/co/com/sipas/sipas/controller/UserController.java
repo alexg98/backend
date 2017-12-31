a1b31e62867ddafd431d41f3645f73d1632dc46c
Alexander Guerrero <alexg98@gmail.com>
2017-12-31 01:03:00 -0500

    Metodo getusers creado

--- src/main/java/co/com/sipas/sipas/controller/UserController.java
+++ src/main/java/co/com/sipas/sipas/controller/UserController.java
@@ -1,6 +1,7 @@
 package co.com.sipas.sipas.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,11 @@ public class UserController {
 		this.userService.save(user);
 		return new RestResponse(HttpStatus.OK.value(), "Operacion exitosa");
 	}
+	
+	@RequestMapping(value ="/getUsers", method = RequestMethod.GET)
+	public List<User> getUsers() {
+		return this.userService.findAll();
+	} 
 	
 	private boolean validate(User user) {
 		boolean isValid = true;
--- src/main/java/co/com/sipas/sipas/service/UserService.java
+++ src/main/java/co/com/sipas/sipas/service/UserService.java
@@ -1,9 +1,22 @@
 package co.com.sipas.sipas.service;
 
+import java.util.List;
+
 import co.com.sipas.sipas.model.User;
 
 public interface UserService {
 
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 */
 	User save(User user);
+	/**
+	 * Recupera la lista de usuarios
+	 * @return lista de usuarios
+	 */
+	List<User> findAll();
+
 
 }
